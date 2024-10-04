23a69c258f0a813e91c2cc59e91c1121ffbabe18
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-04 17:17:41 +0200

    create a function that verify if a number is even and add var computerNumber

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -3,3 +3,16 @@ console.log(choice);
 
 let userNumber = Number(prompt('Inserisci un numero da uno a cinque'));
 console.log(userNumber);
+
+let computerNumber = Math.floor(Math.random() * 5) + 1;
+console.log(computerNumber);
+
+
+//creo una funzione che verifichi se il numero è pari
+function is_even(num) {
+    if (num % 2 == 0) {
+        return true;
+    }
+   
+    return false;
+}
