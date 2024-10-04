2e958ced212dec5abb170fb30ca3f0b80722912f
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-04 18:04:17 +0200

    fix comments in palindroma main.js

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,17 +1,17 @@
 
 //palindromo: parola che al contrario rimane invariata
 
-//FASE 1
+//FASE 1: preparazione 
 //creo una variabile che chieda all'utente di inserire una parola
 let userWord
 
-//FASE 2
+//FASE 2: raccolta Dati
 //assegno un valore alla variabile word
 userWord = prompt('Inserisci una parola');
 console.log(userWord);
 
 
-//FASE 3
+//FASE 3: elaborazione dati
 //creo una funzione che restituisca la parola invertita
 function palindromicWord(word) {
     
@@ -25,7 +25,7 @@ function palindromicWord(word) {
 
 }
 
-
+//verifico se la parola è palindroma
 if (palindromicWord(userWord)) {
     console.log('la parola è palindroma');
     
