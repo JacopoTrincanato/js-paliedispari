bae5c526f7ff4831160e78eba7dbc2ffe51fb30b
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-04 17:37:56 +0200

    add few comments in pari-e-dispari

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,9 +1,12 @@
+//creo una variabile per far scegliere all'utente pari o dispari
 let choice = prompt('Scegli pari o dispari');
 console.log(choice);
 
+//creo una variabile per far inserire all'utente un numero da 1 a 5
 let userNumber = Number(prompt('Inserisci un numero da uno a cinque'));
 console.log(userNumber);
 
+//creo una variabile per far generare un numero random al computer da 1 a 5
 let computerNumber = Math.floor(Math.random() * 5) + 1;
 console.log(computerNumber);
 
@@ -16,6 +19,9 @@ function evenSum(userNumber, computerNumber) {
    
     return false;
 }
+
+console.log(evenSum(userNumber, computerNumber));
+
 
 //stabilisco il vincitore
 if (evenSum(choice)) {
