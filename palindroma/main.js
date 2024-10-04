64bfda0d8a20a39a16f699b056eb78dc52a30bd1
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-04 15:38:38 +0200

    try to create the function

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,5 +1,7 @@
 console.log('ciao');
 
+//palindromo: parola che al contrario rimane invariata
+
 //FASE 1
 //creo una variabile che chieda all'utente di inserire una parola
 let word
@@ -7,3 +9,12 @@ let word
 //FASE 2
 //assegno un valore alla variabile word
 word = prompt('Inserisci una parola');
+
+//FASE 3
+//creo una funzione per capire se la parola è palindroma
+function palindromicWord() {
+    let oppositeWord = word.reverse();
+    return oppositeWord;
+}
+
+console.log(palindromicWord);
