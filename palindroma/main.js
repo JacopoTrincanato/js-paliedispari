3850a0ff892011a2a4196c9c922039a1a4fb3c33
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-04 15:17:34 +0200

    create and initialize word variables

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,1 +1,9 @@
 console.log('ciao');
+
+//FASE 1
+//creo una variabile che chieda all'utente di inserire una parola
+let word
+
+//FASE 2
+//assegno un valore alla variabile word
+word = prompt('Inserisci una parola');
